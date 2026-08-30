ec2c9010d59286b3ac5439e9c879d1ce8112fc7d
rayana barbosa <workcouses00@hotmail.com>
2026-08-30 17:31:09 -0300

    Criar ViewModel da consulta - filtros e resultados do vendedor

---
+++ src/SquadEstoque.Web/Models/ConsultaEstoqueViewModel.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+namespace SquadEstoque.Web.Models;
+
+public class ConsultaEstoqueViewModel
+{
+    public string Termo { get; set; } = string.Empty;
+    public List<ProdutoConsultaResultadoViewModel> Resultados { get; set; } = new();
+    public string? MensagemEstado { get; set; }
+}
+
+public class ProdutoConsultaResultadoViewModel
+{
+    public Guid Id { get; set; }
+    public string Nome { get; set; } = string.Empty;
+    public string Marca { get; set; } = string.Empty;
+    public string Categoria { get; set; } = string.Empty;
+    public string Cor { get; set; } = string.Empty;
+}
